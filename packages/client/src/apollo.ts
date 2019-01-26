eaff2dc31e2748be71498828ce56ac5dfd5e3b23
Jakub Wadas <vadistic@gmail.com>
2019-01-26 11:39:21 +0100

    client/feat: apollo errors

--- packages/client/src/apollo.ts
+++ packages/client/src/apollo.ts
@@ -1,17 +1,26 @@
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient, Initializers } from 'apollo-client'
 import { ApolloLink } from 'apollo-link'
+import { onError } from 'apollo-link-error'
 import { HttpLink } from 'apollo-link-http'
 
 const cache = new InMemoryCache({
   dataIdFromObject: obj => obj.id
 })
 
-const httpLink = new HttpLink({ uri: process.env.ENDPOINT_URL })
+console.log(process.env.REACT_APP_GRAPHQL_ENDPOINT)
+
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT })
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }) => console.error(message))
+  }
+})
 
 export const client = new ApolloClient({
   connectToDevTools: true,
-  link: ApolloLink.from([httpLink]),
+  link: ApolloLink.from([errorLink, httpLink]),
 
   cache
 })
