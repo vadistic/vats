d578f121c982644fb24a84b3e40f9ba3c3212844
Jakub Wadas <vadistic@gmail.com>
2019-01-30 17:14:00 +0100

    build(cosmos): imported cra scripts start setup into cosmos

--- packages/client/cosmos.config.js
+++ packages/client/cosmos.config.js
@@ -1,3 +1,16 @@
+// based on CRA setup
+// node_modules/react-scripts/scripts/start.js
+process.env.BABEL_ENV = 'development'
+process.env.NODE_ENV = 'development'
+
+// Ensure environment variables are read.
+require('react-scripts/config/env')
+
+const verifyTypeScriptSetup = require('react-scripts/scripts/cosmosutils/verifyTypeScriptSetup')
+verifyTypeScriptSetup()
+
+const configFactory = require('react-scripts/config/webpack.config')
+
 // https://github.com/emotion-js/emotion/issues/1123#issuecomment-455767886
 // copy-paste from craco.config.js
 const emotionPresetOptions = {
@@ -17,7 +30,8 @@ module.exports = {
   containerQuerySelector: '#root',
   webpackConfigPath: 'react-scripts/config/webpack.config.js',
   webpack: config => {
-    const _config = config
+    // just use factory
+    const _config = configFactory('development')
     // adding emotion preset to webpack/module/rules/oneOf/(babel-loader for .ts,.tsx)/options/plugins
     // waiting for craco support: https://github.com/sharegate/craco/issues/65
     const plugins = _config.module.rules[2].oneOf[1].options.plugins
