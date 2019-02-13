f5b1ee32c142a39d692e0035d23728fadedd3352
Jakub Wadas <vadistic@gmail.com>
2019-02-13 01:48:16 +0100

    build(server): deps bump & env script

--- packages/server/apollo.config.js
+++ packages/server/apollo.config.js
@@ -1,6 +1,9 @@
-require('dotenv').config()
+require('./scripts/env-config')
 
 module.exports = {
+  service: {
+    localSchemaFile: 'src/generated/server.graphql',
+  },
   engine: {
     apiKey: process.env.ENGINE_API_KEY,
   },
